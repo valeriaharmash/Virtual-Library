4680c1fa6fccf22d9535f1fbb92811c9a558439b
Valeriia Harmash <valeriaharmash@gmail.com>
2023-07-26 15:42:19 -0400

    done

--- classes/Music.js
+++ classes/Music.js
@@ -1,6 +1,27 @@
 // import the Media class:
-
+const Media = require('./Media')
 // create your Music class:
+class Music extends Media {
+	constructor(title, year, genre, artist, length) {
+		super(title, year, genre)
+		this.artist = artist
+		this.length = length
+	}
+	summary() {
+		return `Title: ${this.title}, Artist: ${this.artist}, Year: ${this.year}, Genre: ${this.genre}, Length: ${this.length} seconds`
+	}
+	static shortestAlbum(albums) {
+		let shortestLength = albums[0].length
+		let temp = null
+		for (let i = 1; i < albums.length; i++) {
+			if (albums[i].length < shortestLength) {
+				shortestLength = albums[i].length
+				temp = albums[i]
+			}
+		}
+		return temp
+	}
+}
 
 // don't change below
-module.exports = Music;
+module.exports = Music
